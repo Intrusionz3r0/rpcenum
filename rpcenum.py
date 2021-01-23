0aecbc74a2717cec790485ce5c49b1da26f96154
Luis Adrian Morales Guzman <intrusionz3r0@kali>
2021-01-23 00:28:33 -0600

    rpcenum with credentials

--- rpcenum.py
+++ rpcenum.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#Author: Intrusionz3r0
 import os
 import pyfiglet,sys,signal,argparse
 from prettytable import PrettyTable
@@ -42,7 +43,7 @@ def banner():
 	print(color['red'])
 	print(pyfiglet.figlet_format("RPCENUM"))
 	print(color['off'])
-	print(color['darkblack'] +" "*20+ "By Intrusionz3r0 (v2.0)"+ color['off'])
+	print(color['yellow'] +" "*20+ "By Intrusionz3r0 (v2.0)"+ color['off'])
 
 
 
@@ -55,6 +56,31 @@ def getRIDG(IP):
 			r.append(IP+"[$]"+rgroups[x])
 
 	return r
+
+def getRIDGCredentials(IP,USERNAME,PASSWORD):
+	rgroups = os.popen('rpcclient {} -U "{}%{}" -c "enumdomgroups" | cut -d "[" -f3 | cut -d "]" -f1'.format(IP,USERNAME,PASSWORD)).read().split('\n')
+	r=[]
+
+	for x in range(len(rgroups)):
+		if(rgroups[x] != ""):
+			r.append(IP+"[$]"+USERNAME+"[$]"+PASSWORD+"[$]"+rgroups[x])
+
+	return r
+
+def getGroupCredentials(params):
+	ip = params.split("[$]")[0]
+	user = params.split("[$]")[1]
+	password = params.split("[$]")[2]
+	rid = params.split("[$]")[3]
+
+	group = os.popen('rpcclient {} -U "{}%{}" -c "querygroup {}" | grep "Group Name" | cut -d ":" -f2 | xargs'.format(ip,user,password,rid)).read()
+	group = group.strip()
+
+	desc = os.popen('rpcclient {} -U "{}%{}" -c "querygroup {}" | grep "Description" | cut -d ":" -f2 | xargs'.format(ip,user,password,rid)).read()
+	desc= desc.strip()
+
+	gg = group+"[$]"+desc+"[$]"+rid
+	return gg
 
 
 def getGroup(params):
@@ -99,6 +125,34 @@ def nullsession(IP):
 	printTableG(g)
 	p2.success("finalized.")
 
+
+def rpcenumcredentials(IP,USERNAME,PASSWORD):
+	p1 = log.progress("Users")
+	p1.status("Collecting information from users.")
+
+	request = os.popen('rpcclient {} -U "{}%{}" -c "querydispinfo2" > data.tmp'.format(IP,USERNAME,PASSWORD)).read().split('\n')
+
+	users = os.popen("cat data.tmp | awk '{print $8}'").read().split("\n")
+	rids = os.popen("cat data.tmp | awk '{print $4}'").read().split("\n")
+	names= os.popen("cat data.tmp | awk '{print $1}' FS='Desc:' | awk '{print $2}' FS='Name: ' | sed 's/^[ \t]*//;s/[ \t]*$//'").read().split("\n")
+	desc = os.popen("cat data.tmp | cut -d':' -f7 | sed 's/^[ \t]*//;s/[ \t]*$//'").read().split("\n")
+
+	p1.status("Export users file.")
+	time.sleep(1)
+	writeUsersFile(users)
+	printTableUsers(rids,users,names,desc)
+	p1.success("finalized.")
+	pool = Pool(processes=50)
+	p2 = log.progress("Groups")
+	p2.status("Collecting information from groups.")
+	groupsinfo = getRIDGCredentials(IP,USERNAME,PASSWORD)
+	g=[]
+	for group in pool.imap_unordered(getGroupCredentials,[line for line in groupsinfo]):
+		g.append(group)
+
+	printTableG(g)
+	p2.success("finalized.")
+
 def printTableUsers(rids,users,names,desc):
 	for x in range(len(rids)):
 		if(rids[x] != ""):
@@ -122,7 +176,7 @@ def printTableG(data):
 
 
 def writeUsersFile(users):
-	file = open("users.txt","a")
+	file = open("users.txt","w+")
 	for x in users:
 		if(x != ""):
 			file.write(x+"\n")
@@ -132,14 +186,16 @@ def writeUsersFile(users):
 
 if __name__ == "__main__":
 	banner()
+	print(color['darkwhite'])
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-I", "--ipaddress" , help="specify destination IP address.")
 	parser.add_argument("-U", "--username" , help="set username.")
 	parser.add_argument("-P", "--password" , help="set password.")
 	args = parser.parse_args()
-
+	color['off']
 	if(args.ipaddress and args.username or args.password):
-		print("RPC con credenciales")
+		print("\n")
+		rpcenumcredentials(args.ipaddress,args.username,args.password)
 		sys.exit(0)
 	elif(args.ipaddress):
 		print("\n")
